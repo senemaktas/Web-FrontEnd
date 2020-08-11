e790540bda144ef7a3988f02db5ba3a7dce8a788
senemaktas <30393152+senemaktas@users.noreply.github.com>
2020-08-11 14:36:40 +0300

    Add files via upload

--- RepresentationWithTable/RepresentationWithTable.js
+++ RepresentationWithTable/RepresentationWithTable.js
@@ -1,20 +1,33 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// When the user scrolls the page, execute myFunction - header
+window.onscroll = function() {myFunction()};
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 } 
+
+//Automatic Slideshow
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
